bf233c292ff7a4a26895030a8f9fbd97d07b8d14
preaw59090032 <33709700+preaw59090032@users.noreply.github.com>
2018-02-23 18:45:25 +0700

    Update Code.py

--- Code.py
+++ Code.py
@@ -29,15 +29,3 @@ class Train( Transportation ):
    def find_cost(self):
       return 5*self.station
       
-# main program
-
-travel_cost = 0
-
-trip = [ Walk("KMITL","KMITL SCB Bank",0.6),
-         Taxi("KMITL SCB Bank","Ladkrabang Station",5),
-         Train("Ladkrabang Station","Payathai Station",40,6),
-         Taxi("Payathai Station","The British Council",3) ]
-
-for travel in trip:
-   travel_cost += travel.find_cost()
-print(travel_cost)
